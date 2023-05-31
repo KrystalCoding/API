410fcc0549076bb587f1a78f7e65ceebcd7b042d
KrystalCoding <>
2023-05-31 10:34:48 +0000

    Finishing project and adding Exception Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,49 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-                    "Authorization": API_KEY,
-                 },
-                 body: form,
-        });
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (response.ok) {
-            displayErrors(data);
-        } else {
-            throw new Error(data.error);
-        }
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        displayException(data);
+        throw new Error(data.error);
+    }
 }
 
 function displayErrors(data) {
     let heading = `JSHints Results for ${data.file}`;
 
-if (data.total_errors === 0) {
-    results = `<div class="no_errors">No errors resported!</div>`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors resported!</div>`;
     } else {
         results = `<div>Total Errors: <span class="errors_count">${data.total_errors}</span></div>`
         for (let error of data.error_list) {
@@ -54,6 +72,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -66,7 +85,21 @@ function displayStatus(data) {
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
-    
+
     resultsModal.show();
 }
 
+function displayException(data) {
+
+    let heading = `An Exception Ocurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
+}
